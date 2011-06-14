d896ba1a107a5ee820dc3445ec8115cbfea1ef24
root <root@LHCONE-RMS1.(none)>
2011-06-14 19:24:26 +0100

    Fix hardcoded path.

--- public/js/theme.js
+++ public/js/theme.js
@@ -1,6 +1,6 @@
 
 // directory of where all the images are
-var cmThemeOfficeBase = '/ixp/images/joomla-admin/menu/';
+var cmThemeOfficeBase = '/images/joomla-admin/menu/';
 
 var cmThemeOffice =
 {
